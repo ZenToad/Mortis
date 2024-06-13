1f0612e05be792dcb4410a95fc604e791561f714
assballs <ass@balls>
2024-06-13 13:32:46 -0600

    Adding files

---
+++ mortis.h
@@ -0,0 +1,23 @@
+#ifndef MORTIS_H
+#define MORTIS_H
+
+#endif
+
+#ifdef MORTIS_IMPLEMENTATION
+/*
+ * What even does this need?
+ * I've got lots of but what really do I need
+ * Pretty much every time I need
+ * - memory allocation
+ * - pointer hashmap
+ * - string manipulation
+ *
+ * Is that all I need?  What do I need to start?
+ * We could just ignore the custom allocators?  Do we need that?
+ * I mean I know we may, but now?  and how am I supposed to do that?
+ * I don't even have requirements.  I don't even have a plan.
+ *
+ * Can I just use the stb_ds.h?  
+ *
+ */
+#endif
